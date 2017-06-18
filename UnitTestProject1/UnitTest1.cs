b29674ec26c434eb00e8ddee1f7e74c648beac0f
Johnathon Sullinger <Johnathon@Sullinger.us>
2017-06-18 10:20:32 -0700

    initial repro commit

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            new HttpRequestMessage().CreateResponse(HttpStatusCode.OK);
+        }
+    }
+}
